33c46226fcc8c3a75fe5d2e00a5d1c3303721128
animadhu <35257675+animadhu@users.noreply.github.com>
2019-02-13 15:41:49 +0530

    Add files via upload

---
+++ sorting.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+int *myfunc();
+int main()
+{
+        int *ptr;
+        ptr=myfunc();
+        for(int i=0;i<10;i++)
+        {
+            printf("%d ",*(ptr+i));
+        }
+    return 0;
+}
+int *myfunc()
+{
+    int *b;
+     b=malloc(10*sizeof(int));
+    printf("enter the 10 values");
+    for(int i=0;i<10;i++)
+    {
+        scanf("%d",(b+i));
+    }
+    for(int i=0;i<10;i++)
+    {
+    for(int j=i+1;j<10;j++)
+    {
+        if(*(b+i)> *(b+j))
+        {
+          int t;
+          t=*(b+i);
+          *(b+i)=*(b+j);
+          *(b+j)=t;
+
+        }
+    }
+    }
+    return b;
+}
